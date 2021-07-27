f12d4199d93bdf67587fa72cf029f4a051d6b676
ShuXu <xs@eyedocvision.com>
2021-07-27 16:49:16 +0800

    需求：滚动的后 增加返回顶部按钮

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
+    <el-backtop />
+
     <router-view />
   </div>
 </template>
+
+<style lang="scss" scoped>
+::v-deep .el-backtop {
+  i {
+    color: #7359a4;
+  }
+}
+</style>
 
 <style lang="scss">
 @import '~assets/style';
