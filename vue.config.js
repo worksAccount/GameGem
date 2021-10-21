2769b17442801a9ff849c21fa740691bc11c50a4
worksAccount <majing_12@163.com>
2021-10-21 14:41:24 +0800

    修改网页head

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     index: {
       entry: 'src/main.js', // page 的入口
       template: 'public/index.html', // 模板来源
-      filename: 'index.html', // 在 dist/index.html 的输出
+      filename: 'GameGem.html', // 在 dist/index.html 的输出
       title: 'index', // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
